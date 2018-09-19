85f744260897ec2048f958cf49ac5817593fc873
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-09-19 19:59:29 +0200

    chore: Clean up redundant boilerplate code

--- gui-for-lcp/gui-for-lcp.php
+++ gui-for-lcp/gui-for-lcp.php
@@ -44,27 +44,6 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'GUI_FOR_LCP_VERSION', '1.0.0' );
 
 /**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-gflcp-activator.php
- */
-function activate_gflcp() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-gflcp-activator.php';
-	Gflcp_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-gflcp-deactivator.php
- */
-function deactivate_gflcp() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-gflcp-deactivator.php';
-	Gflcp_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_gflcp' );
-register_deactivation_hook( __FILE__, 'deactivate_gflcp' );
-
-/**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
